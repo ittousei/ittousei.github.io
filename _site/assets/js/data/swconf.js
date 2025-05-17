8ff3fd88dd67e9e316d99345bf7faa2dd169d36f
MsEspeon <missingespeon@gmail.com>
2025-05-18 02:06:40 +0800

    Fix workflow

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1747504557',resources: [
+    cacheName: 'chirpy-1747505155',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
