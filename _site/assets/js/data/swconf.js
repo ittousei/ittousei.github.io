0469df2fd93b5385969d1054512e90dda5a144dc
MsEspeon <missingespeon@gmail.com>
2026-05-27 21:24:54 +0800

    Fix

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1748194543',resources: [
+    cacheName: 'chirpy-1779888128',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
